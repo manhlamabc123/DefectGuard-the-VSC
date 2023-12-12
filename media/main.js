ff63eb752cbb9ddb1570154abdb454d662d92dbf
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-12-12 21:47:58 +0700

    Sort uncommit to top

--- media/main.js
+++ media/main.js
@@ -7,22 +7,45 @@
   const vscode = acquireVsCodeApi();
 
   let commits = [];
+  let languages = ["C", "Python", "Java", "Javascript"];
 
   // Get a reference to the select element
   var languageSelect = document.getElementById("language");
 
+  for (const language of languages) {
+    // Create a new option element
+    var option = document.createElement("option");
+
+    // Set the value and text of the option (customize as needed)
+    option.value = language;
+    option.text = language;
+
+    // Append the option to the select element
+    // @ts-ignore
+    languageSelect.add(option);
+  }
+
   // Add an event listener to the select element
   // @ts-ignore
-  languageSelect.addEventListener("input", function () {
+  languageSelect.addEventListener("change", function () {
     // Get the selected value
     // @ts-ignore
     var selectedLanguage = languageSelect.value;
+
+    // Move the selected language to the top of the list
+    var index = languages.indexOf(selectedLanguage);
+    if (index !== -1) {
+        languages.splice(index, 1);  // Remove the selected language from its current position
+        languages.unshift(selectedLanguage);  // Add it to the beginning of the array
+    }
 
     // Send a message to the extension with the selected language
     vscode.postMessage({
       type: "selectLanguage",
       data: selectedLanguage,
     });
+
+    vscode.setState({ languages: languages });
   });
 
   // Handle messages sent from the extension to the webview
@@ -45,7 +68,11 @@
     ul.textContent = "";
 
     // Sort the commits by probability (descending order)
-    commits.sort((a, b) => parseFloat(b.predict) - parseFloat(a.predict));
+    commits.sort((a, b) => {
+        if (a.commit_hash === "uncommit") return -1;
+        if (b.commit_hash === "uncommit") return 1;
+        return parseFloat(b.predict) - parseFloat(a.predict);
+    });
 
     for (const commit of commits) {
       const predictValue = parseFloat(commit.predict);
@@ -53,7 +80,7 @@
       let predict, hue;
 
       if (predictValue === -1) {
-          // Handle the case when commit.predict is -1
+          // Handle the case when commit.predict is "uncommit"
           hue = '0';
           predict = 'No Code Changes';  // Or any other default value you prefer
       } else {
